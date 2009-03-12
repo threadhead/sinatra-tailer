c7551b07c95363c8d9f11e3daf76ec46c6afc4c6
Karl Smith <threadhead@gmail.com>
2009-03-12 13:35:14 -0700

    moved js into application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,54 @@
-// site-wide scripts
+$(document).ready(function(){
+	var selectedLog;
+	function setSelectedLog() {
+		selectedLog = $("#select_log option:selected").val();
+	}
+	
+	$("#select_log").change(function(e){
+		setSelectedLog();
+		window.parent.location = "/log/" + selectedLog;
+	});
+	
+	
+	function updateLog() {
+		setSelectedLog();
+		$("#log_text").load("/log/" + selectedLog + "/log_text");
+	}
+	
+	
+	$("#refresh_seconds").change( function(){
+		stopUpdate();
+		startUpdate();
+	});
+	
+	
+	var intervalID;
+	function startUpdate() {
+		var updateInterval = $("#refresh_seconds option:selected").val() * 1000;
+		intervalID = setInterval( function(){updateLog();}, updateInterval);
+	}
+	
+	function stopUpdate() {
+		clearInterval(intervalID);
+	}
+	
+	
+	$("button[id='start-stop']").click(function(){
+		if ($(this).html() == "start") {
+			$("#refresh_seconds").removeAttr("disabled");
+			$("#spinner").show();
+			$(this).html("stop");
+			startUpdate();
+			
+		} else {
+			$("#refresh_seconds").attr("disabled","disabled");
+			$("#spinner").hide();
+			$(this).html("start");
+			stopUpdate();
+			
+		};
+	});
+	
+	updateLog();
+	startUpdate();
+});
