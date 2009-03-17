49850454a2f97efb138dcf2fe34383208d4308be
Karl Smith <threadhead@gmail.com>
2009-03-16 21:40:01 -0700

    moved functions out of doc.read in application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,33 @@
+var selectedLog;
+var logLines;
+var updateSeconds;
+function setSelectedLog() {
+	selectedLog = $("#select_log option:selected").val();
+	logLines = $("#log_lines option:selected").val();
+	updateSeconds = $("#refresh_seconds option:selected").val();
+}
+
+
+function updateLog() {
+	setSelectedLog();
+	$("#log_text").load("/log_text", {lines: logLines, id: selectedLog, refresh: updateSeconds} );
+	$("#path-info").load("/info_bar", {lines: logLines, id: selectedLog, refresh: updateSeconds} );
+}
+
+
+var intervalID;
+function startUpdate() {
+	setSelectedLog();
+	updateInterval = updateSeconds * 1000;
+	intervalID = setInterval( function(){updateLog();}, updateInterval);
+}
+
+function stopUpdate() {
+	clearInterval(intervalID);
+}
+
+
 $(document).ready(function(){
-	var selectedLog;
-	var logLines;
-	var updateSeconds;
-	function setSelectedLog() {
-		selectedLog = $("#select_log option:selected").val();
-		logLines = $("#log_lines option:selected").val();
-		updateSeconds = $("#refresh_seconds option:selected").val();
-	}
-	
 	$("#select_log").change(function(e){
 			setSelectedLog();
 			window.parent.location = "/log/" + selectedLog;
@@ -18,30 +38,11 @@ $(document).ready(function(){
 	});
 	
 	
-	function updateLog() {
-		setSelectedLog();
-		$("#log_text").load("/log_text", {lines: logLines, id: selectedLog, refresh: updateSeconds} );
-		$("#path-info").load("/info_bar", {lines: logLines, id: selectedLog, refresh: updateSeconds} );
-	}
-	
-	
 	$("#refresh_seconds").change( function(){
 		stopUpdate();
 		startUpdate();
 	});
-	
-	
-	var intervalID;
-	function startUpdate() {
-		setSelectedLog();
-		updateInterval = updateSeconds * 1000;
-		intervalID = setInterval( function(){updateLog();}, updateInterval);
-	}
-	
-	function stopUpdate() {
-		clearInterval(intervalID);
-	}
-	
+		
 	
 	$("button[id='start-stop']").click(function(){
 		if ($(this).html() == "start") {
