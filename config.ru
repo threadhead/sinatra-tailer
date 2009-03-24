8011b030b24f19c68dd7ecb8f06305e2ab863148
Karl Smith <threadhead@gmail.com>
2009-03-24 09:58:42 -0700

    more compatible with rackup and ruby startup

--- config.ru
+++ config.ru
@@ -1,4 +1,2 @@
-LIB_PATH = File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
-$:.unshift LIB_PATH
 require 'rack_logs'
-run RackLogs.new
+run Sinatra::Application
--- rack_logs.rb
+++ rack_logs.rb
@@ -1,1 +1,49 @@
-lib/rack_logs.rb
+require 'rubygems'
+require 'erb'
+require 'sinatra'
+require 'pp'
+require 'lib/logs'
+
+# set :logging, false
+enable :sessions
+
+
+before do
+  @logs = $logs    
+  session['lines'] ||= "15"
+  session['refresh'] ||= "3"
+  
+  session['lines'] = params['lines'] if params['lines']
+  session['refresh'] = params['refresh'] if params['refresh']
+end
+
+get '/' do
+  redirect '/log/0'
+end
+
+
+get '/log/:id' do
+  @log = @logs[params['id'].to_i]
+  @log_text = @log.get_some_log( session['lines'] )
+  erb :log
+end
+
+
+post '/log_text' do
+  @log = @logs[params['id'].to_i]
+  @log_text = @log.get_some_log( session['lines'] )
+  erb :log_text
+end
+
+  
+post '/info_bar' do
+  @log = @logs[params['id'].to_i]
+  erb :path_info
+end
+
+
+configure do
+  $logs = []
+  logs_config = Logs.load_config_file
+  logs_config.each{ |log| $logs += Logs.add_logs(log)}
+end
